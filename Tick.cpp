60075ea300d164c313381e74be2475e127591ad1
Gregg H <50878128+GreggZumbari@users.noreply.github.com>
2019-10-02 10:48:19 -0700

    End of 10/2/2019

---
+++ Tick.cpp
@@ -0,0 +1,9 @@
+/*
+ * This program is a console-bound tic-tac-toe game, where two players on the same
+ * computer may take turns, playing their respective marks on the 3x3 grid board. It
+ * counts how many wins and ties each side has achieved after each round.
+ * @author Greggory Hickman, September-October 2019
+ * @version pre-release 1.0
+ */
+#include <iostream>
+#include <cstring>

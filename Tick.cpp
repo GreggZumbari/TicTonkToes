dd72f4dc3453e8cd620776acdcfb8485a7d28218
GreggH <greggory.png@gmail.com>
2019-10-09 20:56:12 -0700

    Board works correctly
    Mark my words, this took more effort than it should have

--- Tick.cpp
+++ Tick.cpp
@@ -13,67 +13,87 @@ using namespace std;
 int main() {
   
   //Player moves and empty values defined as constants to make code more readable
-  char BLANK = (char)32;
-  char X_MOVE = 'X';
-  char O_MOVE = 'O';
-
-  //Whose turn is it?
-  char turn = X_MOVE;
+  int O_MOVE = 0;
+  int X_MOVE = 1;
+  int BLANK = 2;
 
   //Player command
   char move[2];
-
-  int boardSize = 3;
+    
   int turnCount = 0;
 
   //Holds data for each tile
-  int board[boardSize][boardSize];
+  int board[3][3];
   
-  //Clear board
-  for (int x = 1; x < boardSize; x++) {
-    for (int y = 1; y < boardSize; y++) {
-      board[x][y] = BLANK;
-    }
-  }
-  
-  cout << "Hello fellow friendless person. This is my 2 player tic-tac-toe game." << endl;
+  cout << "Hello all. This is my 2 player tic-tac-toe game." << endl;
   cout << "To place your piece, type the corresponding row number, followed by the corresponding column number on your turn." << endl;
   cout << "For example, type \"2b\" to play in the middle square, 1c to play in the top-right square, etc." << endl;
   cout << endl << "---------------" << endl;
 
   while (true) {
-
-    cout << endl;
-    
-    //Print the board
-    cout << "Turns elapsed: " << turnCount << endl;
-    cout << "  a b c" << endl;
-    for (int x = 0; x < boardSize; x++) {
-      cout << x + 1;
-      for (int y = 0; y < boardSize; y++) {
-	cout << " " << (char)board[x][y];
+     
+    bool playerOneVictory = false;
+    bool playerTwoVictory = false;
+      
+    int turn = X_MOVE;
+      
+    //Clear board
+    for (int x = 0; x < 3; x++) {
+      for (int y = 0; y < 3; y++) {
+        board[x][y] = BLANK;
       }
-    cout << endl;
     }
 
-    //Place a piece
-    cin >> move;
-    if (((move[0] == '1' || move[0] == '2' || move[0] == '3') && (move[1] == 'a' || move[1] == 'b' || move[1] == 'c')) ) {
-      if (turn == X_MOVE) {
-	board[move[0] - 49][move[1] - 97] = X_MOVE;
-	turn = O_MOVE;
+    cout << endl;
+      
+    while (!playerOneVictory && !playerTwoVictory) {
+      
+      //Print the board
+      cout << "Turns elapsed: " << turnCount << endl;
+      cout << "  a b c" << endl;
+      for (int x = 0; x < 3; x++) {
+	cout << x + 1;
+	for (int y = 0; y < 3; y++) {
+	  cout << ' ';
+	  if (board[x][y] == 2) {
+	    cout << ' ';
+	  }
+	  if (board[x][y] == 1) {
+	    cout << 'X';
+	  }
+	  if (board[x][y] == 0) {
+	    cout << 'O';
+	  }
+	}
+	cout << endl;
+      }
+      if (turn == 1) {
+	cout << "It is X\'s turn!" << endl;
       }
       else {
-	board[move[0] - 49][move[1] - 97] = O_MOVE;
-	turn = X_MOVE;
+	cout << "It is O\'s turn!" << endl;
       }
-      turnCount++;
-    }
-    else {
-      cout << "Illegal move. Please enter a number, followed by a letter (e.x. \"2a\", \"1c\", \"3b\")" << endl; 
+
+      //Place a piece
+      cin >> move;
+      cout << endl;
+      if (((move[0] == '1' || move[0] == '2' || move[0] == '3') && (move[1] == 'a' || move[1] =='b' || move[1] == 'c')) ) {
+        if (turn == X_MOVE) {
+	  board[move[0] - 49][move[1] - 97] = X_MOVE;
+	  turn = O_MOVE;
+        }
+        else if (turn == O_MOVE) {
+	  board[move[0] - 49][move[1] - 97] = O_MOVE;
+	  turn = X_MOVE;
+        }
+        turnCount++;
+      }
+        else {
+          cout << "Illegal move. Please enter a number, followed by a letter (e.x. \"2a\", \"1c\", \"3b\")" << endl;
+        }
+      
     }
 
   }
   
 }
-
